6404344a193fc4a4f31144c646a81d4a654c34e0
ksiushamalko <malkovitch.ksenia@yandex.ru>
2026-04-22 21:15:24 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,18 +2,56 @@ import sqlite3
 
 class Recipe:
     def __init__(self, id, name, instructions):
-        self.id = id
-        self.name = name
-        self.instructions = instructions
+        self._id = id          
+        self._name = name      
+        self._instructions = instructions 
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def instructions(self):
+        return self._instructions
 
     def __str__(self):
-        return f"{self.name}: {self.instructions}"
+        return f"{self._name}: {self._instructions}"
+
+
+class DatabaseConnection:
+    _instance = None
+    _conn = None
+    _cursor = None
+
+    def __new__(cls, db_name='fridge.db'):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._conn = sqlite3.connect(db_name)
+            cls._instance._conn.row_factory = sqlite3.Row
+            cls._instance._cursor = cls._instance._conn.cursor()
+        return cls._instance
+
+    def get_connection(self):
+        return self._conn
+
+    def get_cursor(self):
+        return self._cursor
+
+    def close(self):
+        if self._conn:
+            self._conn.close()
+            DatabaseConnection._instance = None
+
 
 class Database:
-    def __init__(self, db_name='fridge.db'):
-        self.conn = sqlite3.connect(db_name)
-        self.conn.row_factory = sqlite3.Row
-        self.cursor = self.conn.cursor()
+    def __init__(self):
+        self._db = DatabaseConnection()
+        self._conn = self._db.get_connection()
+        self._cursor = self._db.get_cursor()
 
     def find_recipes(self, user_products):
         if not user_products:
@@ -21,21 +59,21 @@ class Database:
         
         user_products_lower = [p.lower().strip() for p in user_products]
         
-        self.cursor.execute('SELECT id, name, instructions FROM recipes')
-        all_recipes = self.cursor.fetchall()
+        self._cursor.execute('SELECT id, name, instructions FROM recipes')
+        all_recipes = self._cursor.fetchall()
         
         result = []
         for recipe in all_recipes:
             recipe_id = recipe['id']
             
-            self.cursor.execute('''
+            self._cursor.execute('''
                 SELECT p.name 
                 FROM recipe_products rp
                 JOIN products p ON rp.product_id = p.id
                 WHERE rp.recipe_id = ?
             ''', (recipe_id,))
             
-            required_products = [row['name'].lower() for row in self.cursor.fetchall()]
+            required_products = [row['name'].lower() for row in self._cursor.fetchall()]
             
             has_all = True
             for prod in required_products:
@@ -49,8 +87,8 @@ class Database:
         return result
 
     def get_all_products(self):
-        self.cursor.execute('SELECT name FROM products ORDER BY name')
-        return [row['name'] for row in self.cursor.fetchall()]
+        self._cursor.execute('SELECT name FROM products ORDER BY name')
+        return [row['name'] for row in self._cursor.fetchall()]
 
     def close(self):
-        self.conn.close()
+        self._db.close()
